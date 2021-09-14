0b4a73dbb26c619c2d2281c44d427cb70482124b
Your Name <you@example.com>
2021-09-14 09:24:50 +0700

    Add project files.

---
+++ WebApplication1/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PTB1.Models
+{
+    [Table("Accounts")]
+    public class Account
+    {
+        [Key]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PTB1.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        public string StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+}
