7cbfb020b68a91067c01dd0f37d26a540563ddc0
Your Name <you@example.com>
2021-09-21 09:29:58 +0700

    up

---
+++ WebApplication1/Models/sinhvien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class sinhvien
+    {
+        [Key]
+        public string hovaten { get; set; }
+        public string school { get; set; }
+    }
+}
